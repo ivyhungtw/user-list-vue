17a372cf791d23bf8c8b6638a299527f8a5e66ad
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-07-25 11:18:37 -0500

    feat: show user's location

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = {
     },
     async fetchUsers() {
       const result = await axios.get(
-        'https://randomuser.me/api/?inc=gender,name,nat,picture,email,login,dob&results=20'
+        'https://randomuser.me/api/?inc=gender,name,nat,picture,email,login,dob,location&results=20'
       )
 
       const users = result.data.results
@@ -26,7 +26,8 @@ const app = {
         email: user.email,
         gender: user.gender,
         birthday: user.dob.date.substring(0, user.dob.date.indexOf('T')),
-        age: user.dob.age
+        age: user.dob.age,
+        googleMapUrl: `https://maps.google.com/maps?f=q&geocode=&q=${user.location.coordinates.latitude},${user.location.coordinates.longitude}&z=10&output=embed`
       }))
     }
   },
